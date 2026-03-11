974953c8c8b344f004c964d6c16063e5ee910456
Sudarshan Gorakhnath Phule <sgphule@gmail.com>
2026-03-11 04:43:01 +0100

    structure contents in hierarchy

--- 09Runnables/lcel.py
+++ 09Runnables/lcel.py
@@ -7,11 +7,11 @@ from langchain_core.runnables import RunnableSequence
 load_dotenv()
 
 prompt1 = PromptTemplate(
-    template = "List down {topic} in LangChain Open Source framework for implementing AI agents",
+    template = "List down {topic} of Germany",
     input_variables = ["topic"]
 )
 model = ChatOpenAI()
 parser = StrOutputParser()
 lcel = prompt1 | model | parser
-result = lcel.invoke({"topic":"Document loaders"})
+result = lcel.invoke({"topic":"states"})
 print(result)
